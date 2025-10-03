ae77b408e0e39d659f7a41bf7629cbbdce1205b1
corneldlabratt <onyekpecornelius20@gmail.com>
2025-10-03 22:57:27 +0100

    new questions added

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -56,6 +56,110 @@ export const questions: QuestionsByDifficulty = {
     options: ['56k', '20k', '100k', '40k'],
     correctAnswer: '56k'
   },],
-  medium: [],
-  hard: []
+
+
+  medium: [{
+    id: 11,
+    question: 'What kind of scalability solution is Citrea on Bitcoin?',
+    options: ['Sidechain', 'State channel', 'ZK rollup', 'Plasma'],
+    correctAnswer: 'ZK rollup'
+  }, {
+    id: 12,
+    question: 'Which crypographic technology does citrea use to enhance bitcoin blockspace?',
+    options: ['Multisignatures', 'Haslocks', 'Zero-knowledge proofs', 'Threshold signatures'],
+    correctAnswer: 'Zero-knowledge proofs'
+  }, {
+    id: 13,
+    question: 'What two roles does Bitcoin play in Citrea\'s architecture?',
+    options: ['Consensus and governance', 'Execution and staking', 'Data availability and Settlement', 'Sequencing and Validation'],
+    correctAnswer: 'Data availability and Settlement'
+  }, {
+    id: 14,
+    question: 'What\'s the main purpose of citrea\'s zk rollup design?',
+    options: ['To replace bitcoin\'s consensus', 'To scale bitcoin while preserving security', 'To build a mew L1 blockchain', 'To reduce miner rewards'],
+    correctAnswer: 'To scale bitcoin while preserving security'
+  }, {
+    id: 15,
+    question: 'citrea testnet transaction are ultimately secured by?',
+    options: ['Ethereum', 'Solana', 'Bitcoin', 'Polygon'],
+    correctAnswer: 'Bitcoin'
+  }, {
+    id: 16,
+    question: 'Citrus champion candidate role is given at what level?',
+    options: ['20', '15', '9', '12'],
+    correctAnswer: '15'
+  }, {
+    id: 17,
+    question: 'To get citrus champion you need to pass a quiz?',
+    options: ['True', 'False'],
+    correctAnswer: 'True'
+  }, {
+    id: 18,
+    question: '___________ is the first owned bitcoin financial platform?',
+    options: ['Zentra', 'Satsuma', 'Tanari', 'Crest'],
+    correctAnswer: 'Tanari'
+  }, {
+    id: 19,
+    question: '___________ is the citrea dex?',
+    options: ['Satsuma', 'DZap', 'Signal', 'Juice Swap'],
+    correctAnswer: 'Satsuma'
+  }, {
+    id: 20,
+    question: 'How many ₿apps are live on citre testnet?',
+    options: ['10', '15', '7', '18'],
+    correctAnswer: '15'
+  },],
+
+
+  hard: [{
+    id: 21,
+    question: 'How do ₿apps contribute to Bitcoin?',
+    options: ['high gas fees', 'They expand what you can do with BTC', 'nothing special', '🤔🤔'],
+    correctAnswer: 'They expand what you can do with BTC'
+  }, {
+    id: 22,
+    question: 'How does Citrea ensure data availability for its rollup transactions?',
+    options: ['Using external DA networks like Celestia', 'Posting transaction commitments and calldata directly to Bitcoin', 'Relying on sequencer honesty', 'Through Ethereum DA layers'],
+    correctAnswer: 'Posting transaction commitments and calldata directly to Bitcoin'
+  }, {
+    id: 23,
+    question: 'Citrea’s Clementine bridge design leverages which Bitcoin scripting upgrade for fraud proof verification?',
+    options: ['SegWit v0', 'Taproot', 'OP_RETURN', 'OP_CHECKTEMPLATEVERIFY (CTV)'],
+    correctAnswer: 'Taproot'
+  }, {
+    id: 24,
+    question: 'Why is Citrea considered “EVM equivalent” rather than just “EVM compatible”?',
+    options: ['It requires a custom compiler for Solidity', 'It reproduces Ethereum’s execution environment without changes', 'It removes gas limits from EVM', 'It runs Solidity but not Vyper'],
+    correctAnswer: 'It reproduces Ethereum’s execution environment without changes'
+  }, {
+    id: 25,
+    question: 'In Citrea, who produces zk-proofs for rollup batches?',
+    options: ['Sequencers only', 'Independent provers', 'Bitcoin miners', 'Ethereum validators'],
+    correctAnswer: 'Independent provers'
+  }, {
+    id: 26,
+    question: 'What risk does Clementine mitigate by using BitVM-based verification?',
+    options: ['Censorship of sequencers', 'Custodial bridges', 'Miner extractable value (MEV)', 'Proof-of-stake centralization'],
+    correctAnswer: 'Custodial bridges'
+  }, {
+    id: 27,
+    question: 'During its Series A raise, Citrea emphasized a vision of Bitcoin becoming more than “digital gold.” What key narrative was used?',
+    options: ['Bitcoin as the settlement layer for global trade', 'Bitcoin as a universal data availability layer', 'Bitcoin as a base for scalable smart contract apps', 'Bitcoin as an energy-backed reserve'],
+    correctAnswer: 'Bitcoin as a base for scalable smart contract apps'
+  }, {
+    id: 28,
+    question: 'If a sequencer proposes an invalid state transition, how is it handled in Citrea?',
+    options: ['Slashed via a staking mechanism', 'Ignored, but users must reorganize', 'zk-proofs fail verification, preventing finality on Bitcoin ', 'Bitcoin miners manually review proofs'],
+    correctAnswer: 'zk-proofs fail verification, preventing finality on Bitcoin '
+  }, {
+    id: 29,
+    question: ' Which team is the core builder of Citrea?',
+    options: ['Chainway', 'StarkWare', 'Matter Labs', 'Offchain Labs'],
+    correctAnswer: 'Chainway'
+  }, {
+    id: 30,
+    question: 'OG role cost how many CM ? ',
+    options: ['10,000', '20,000', '40,000', '80,000'],
+    correctAnswer: '80,000'
+  },]
 };
